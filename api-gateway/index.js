4556e9ccedb1a92c16f6d398c6d28aa6d533b181
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2022-01-08 03:00:23 +0700

    [ADD] /trip api

---
+++ api-gateway/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const port = 8080;
+
+app.get("/trips", async (req, res) => {
+  let { data: trips } = await axios.get("http://localhost:9000/trips");
+  let query = req.query;
+  let keyword = "";
+  if (query.keyword) keyword = query.keyword;
+  let words = keyword.trim().split(" ");
+
+  const filteredTrips = trips.filter((trip) => {
+    trip.weight = 0;
+    return words.every((word) => {
+      tag = trip.tags.includes(word);
+      tag && (trip.weight += 3);
+      title = trip.title.includes(word);
+      title && (trip.weight += 2);
+      description = trip.description.includes(word);
+      description && (trip.weight += 1);
+
+      return tag || title || description;
+    });
+  });
+
+  res.json(filteredTrips.sort((a, b) => b.weight - a.weight));
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
