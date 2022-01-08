067688d54e30ad4fc6d2b05349f5d475882e257f
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2022-01-09 03:51:00 +0700

    [ADD] test script

---
+++ api-gateway/trips.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const app = require("./app.js");
+
+describe("Test trips API", () => {
+  test("It should response status 200", async () => {
+    const response = await request(app).get("/trips");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should response status 200", async () => {
+    const response = await request(app).get("/trips?keyword=");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should response status 200", async () => {
+    const response = await request(app).get("/trips?keyword=Hakone");
+    console.log(response.data);
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should response status 404", async () => {
+    const response = await request(app).get("/trips?keyword=abcdefg");
+    expect(response.statusCode).toBe(404);
+  });
+});
