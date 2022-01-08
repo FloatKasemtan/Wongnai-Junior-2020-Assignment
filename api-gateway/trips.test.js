9bfefcf3fcb26ceb7ac8fa3132fd5be9ff3cb393
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2022-01-09 04:02:55 +0700

    [ADD] testcase

--- api-gateway/trips.test.js
+++ api-gateway/trips.test.js
@@ -19,4 +19,9 @@ describe("Test trips API", () => {
     const response = await request(app).get("/trips?keyword=abcdefg");
     expect(response.statusCode).toBe(404);
   });
+
+  test("It should response status 404", async () => {
+    const response = await request(app).get("/trips?keyword=1230");
+    expect(response.statusCode).toBe(404);
+  });
 });
